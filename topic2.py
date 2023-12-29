19bfabb5da298fcde3e636880eb05fc812e82646
knot <2415080994@qq.com>
2023-12-29 23:48:01 +0800

    完成题目二

---
+++ topic2.py
@@ -0,0 +1,21 @@
+import cv2 as cv
+
+img = cv.imread("f2.jpg")
+kernel = cv.getStructuringElement(cv.MORPH_RECT,(9,9))
+img1 = cv.morphologyEx(img,cv.MORPH_OPEN,kernel)
+img2 = cv.cvtColor(img1,cv.COLOR_BGR2GRAY)
+retval,img3 = cv.threshold(img2,150,255,cv.THRESH_BINARY)
+cv.imshow('img3',img3)
+num_labels,labels,stats,centroids = cv.connectedComponentsWithStats(img3,connectivity=8,ltype=None)
+print(num_labels)
+print(stats)
+for i in range(0,num_labels):
+    if stats[i][0] !=0:
+        x = stats[i][0]
+        y = stats[i][1]
+        w = stats[i][2]
+        h = stats[i][3]
+        cv.rectangle(img,(x,y),(x+w,y+h),(0,0,255),3)
+cv.imshow('img',img)
+cv.waitKey(0)
+cv.destroyAllWindows()
